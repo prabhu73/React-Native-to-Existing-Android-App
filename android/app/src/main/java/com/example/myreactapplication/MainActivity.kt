9ecbee334ec90258f146e99c05ec2af8630bb6cc
Prabhudas Kuppla <prabhudaskuppla@FPTECHS095s-MacBook-Pro.local>
2018-11-12 12:31:57 +0530

    First Commit

---
+++ android/app/src/main/java/com/example/myreactapplication/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.example.myreactapplication
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.support.v7.app.AppCompatActivity
+import android.view.KeyEvent
+import android.widget.Toast
+import com.facebook.react.ReactInstanceManager
+import com.facebook.react.ReactRootView
+import com.facebook.react.common.LifecycleState
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
+import com.facebook.react.shell.MainReactPackage
+
+
+class MainActivity : AppCompatActivity(), DefaultHardwareBackBtnHandler {
+
+    private val OVERLAY_PERMISSION_REQ_CODE: Int = 1
+    private lateinit var mReactRootView: ReactRootView
+    private lateinit var mReactInstanceManager: ReactInstanceManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mReactRootView = ReactRootView(this)
+        mReactInstanceManager = ReactInstanceManager.builder()
+            .setApplication(application)
+            .setBundleAssetName("index.android.bundle")
+            .setJSMainModulePath("index")
+            .addPackage(MainReactPackage())
+            .setUseDeveloperSupport(BuildConfig.DEBUG)
+            .setInitialLifecycleState(LifecycleState.RESUMED)
+            .build()
+        // The string here (e.g. "MyReactNativeApp") has to match
+        // the string in AppRegistry.registerComponent() in index.js
+        mReactRootView.startReactApplication(mReactInstanceManager, "MyReactApplication", null)
+        setContentView(mReactRootView)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                val intent = Intent(
+                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:$packageName")
+                )
+                startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE)
+            }
+        }
+    }
+
+    override fun invokeDefaultOnBackPressed() {
+        super.onBackPressed()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!Settings.canDrawOverlays(this)) {
+                    Toast.makeText(this, "Not configured", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+        mReactInstanceManager.onActivityResult(this, requestCode, resultCode, data);
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostPause(this)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostResume(this, this)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostDestroy(this)
+        }
+        if (mReactRootView != null) {
+            mReactRootView.unmountReactApplication()
+        }
+    }
+
+    override fun onBackPressed() {
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onBackPressed()
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
+            mReactInstanceManager.showDevOptionsDialog()
+            return true
+        }
+        return super.onKeyUp(keyCode, event)
+    }
+}
